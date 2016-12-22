497a4ce9813078358b19ce1cbcef680f6c9cd143
dhowe <github.utensil@mailhero.io>
2016-12-22 19:52:54 +0800

    removed regex check on queryString, handles case when location is programmatically updated

--- js/background.js
+++ js/background.js
@@ -1,210 +1,215 @@
-var showLogs = true;
+var showLogs = false;
 
 var disabled = {},
-    gfw = 'http://www.greatfirewallofchina.org',
-    qRegex = /q=([^&#]*)|p=([^&#]*)/g,
-    regexes = ['^(www\.)*google\.((com\.|co\.|it\.)?([a-z]{2})|com)$', '^(www\.)*bing\.(com)$', '^(([a-z]{2})\.search)|search\.yahoo\.com$'],
-    hostRegex = new RegExp(regexes.join("|"), "i"),
-    triggers = ['shang+fulin+graft', 'zhang+yannan', 'celestial+empire', 'grass+mud+horse', '草泥'];
+  gfw = 'http://www.greatfirewallofchina.org',
+  triggers = ['shang+fulin+graft', 'zhang+yannan', 'celestial+empire', 'grass+mud+horse', '草泥'],
+  engines = ['^(www\.)*google\.((com\.|co\.|it\.)?([a-z]{2})|com)$', '^(www\.)*bing\.(com)$', '^(([a-z]{2})\.search)|search\.yahoo\.com$'];
 
-chrome.runtime.onInstalled.addListener(function(event) {
-    init();
+chrome.runtime.onStartup.addListener(updateCheck);
 
+chrome.runtime.onInstalled.addListener(function () {
+  loadList();
 });
 
-chrome.runtime.onStartup.addListener(function(event) {
-    updateCheck();
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  //console.log('onUpdated', tabId, changeInfo, tab);
+  if (changeInfo && changeInfo.status == "complete") {
+    chrome.tabs.sendMessage(tabId, {
+      what: 'tabUpdate',
+      url: tab.url
+    });
+  }
 });
 
-chrome.runtime.onMessage.addListener(function(request, sender, callback) {
+chrome.runtime.onMessage.addListener(function (request, sender, callback) {
 
-    showLogs && console.log("onMessage:", request.what, request);
+  if (request.what === "checkPage") {
 
-    if (request.what === "checkPage") { // from content_script
+    if (typeof disabled[sender.tab.id] !== 'undefined') {
 
-        if (typeof disabled[sender.tab.id] !== 'undefined') {
+      setTimeout(function () {
+        delete disabled[sender.tab.id];
+      }, 5000); // remove after 5 seconds
 
-            setTimeout(function() {
-                delete disabled[sender.tab.id];
-            }, 5000); // remove after 5 seconds
-
-            callback({
-                status: 'disabled'
-            });
-        }
-
-        // do we have a search, check the query term
-        var host = request.location.host, keyword = "",
-            matches = qRegex.exec(request.location.search);
-
-        if(matches && matches.length > 0) keyword = matches[0].substring(2);
-
-        if(keyword == ""){
-             hashinfo = request.location.hash;
-             matches = qRegex.exec(hashinfo);
-             if(matches && matches.length > 0) keyword = matches[1];
-        }
-
-        showLogs && console.log("matches:" + matches);
-
-        showLogs && console.log("host", host, "keyword", keyword);
-        showLogs && console.log("search engine?", hostRegex.test(host));
-
-        if (hostRegex.test(host) && keyword !== null && keyword != "") {
-
-            var query = decodeURI(keyword.toLowerCase());
-            showLogs && console.log('search: ' + query);
-
-            for (var i = 0; i < triggers.length; i++) {
-
-                if (query === triggers[i].toLowerCase()) {
-
-                    showLogs && console.log('block: ' + query);
-                    callback({
-                        status: 'block',
-                        trigger: query
-                    });
-                    return true;
-                }
-            }
-
-        }
-
-        // otherwise check with china servers
-        checkPage(sender.tab, callback);
-
-    } else if (request.what === "disablePage") { // from popup
-
-        // add to disabled-tabId table
-        disabled[request.tabId] = true;
-
-        // and reload (will trigger content-script and be ignored)
-        chrome.tabs.reload(request.tabId);
+      callback && callback({
+        status: 'disabled'
+      });
     }
 
-    return true;
+    var hostRegex = new RegExp(engines.join('|'), 'i'),
+      keyvals = keysValues(request.location.href);
+    keyword = keyvals.q || keyvals.p;
+
+    if (keyword && keyword.length && hostRegex.test(request.location.host)) {
+
+      var query = decodeURI(keyword.toLowerCase());
+      showLogs && console.log('search: ' + query);
+
+      for (var i = 0; i < triggers.length; i++) {
+
+        if (query === triggers[i]) {
+
+          showLogs && console.log('block: ' + query);
+
+          callback({
+            status: 'block',
+            trigger: query
+          });
+
+          return; // got one, we're done
+        }
+      }
+    }
+
+    // otherwise check with china servers
+    checkPage(sender.tab, callback);
+
+  } else if (request.what === "disablePage") { // from popup
+
+    console.log("disable page! ");
+
+    // add to disabled-tabId table
+    disabled[request.tabId] = true;
+
+    // and reload (will trigger content-script and be ignored)
+    chrome.tabs.reload(request.tabId);
+  }
 });
 
 /**************************** functions ******************************/
 
-var checkPage = function(tab, callback) {
+function keysValues(href) {
 
-    showLogs && console.log('checkPage:', tab.url);
+  var vars = [],
+    hashes = href.slice(href.indexOf('?') + 1).split('&');
+  for (var i = 0; i < hashes.length; i++) {
+    var hash = hashes[i].split('=');
+    vars.push(hash[0]);
+    vars[hash[0]] = hash[1];
+  }
 
-    $.ajax(gfw + '/index.php?siteurl=' + tab.url, {
-        success: function(data) {
-            callback(parseResults(data));
-        },
-        error: function(e) {
-            callback({
-                status: 'error',
-                fails: -1
-            });
-            console.warn(e);
-        }
-    });
+  return vars;
 }
 
-var parseResults = function(html) {
+var checkPage = function (tab, callback) {
 
-    var fails = 0,
-        result = {};
+  showLogs && console.log('checkPage:', tab.url);
 
-    // remove img tags before calling find
-    html = html.replace(/<img\b[^>]*>/ig, '');
-
-    var locs = $(html).find('.resultlocation'),
-        vals = $(html).find('.resultstatus');
-
-    for (var i = 0; i < locs.length; i++) {
-        var value = $(vals[i]).text().toLowerCase();
-        result[$(locs[i]).text().toLowerCase()] = value;
-        if (value === 'fail') fails++;
+  $.ajax(gfw + '/index.php?siteurl=' + tab.url, {
+    success: function (data) {
+      callback(parseResults(data));
+    },
+    error: function (e) {
+      callback({
+        status: 'error',
+        fails: -1
+      });
+      console.warn(e);
     }
-
-    result.status = fails > 2 ? 'block' : 'allow';
-    //console.log('result:', locs.length, vals.length, result);
-    return result;
+  });
 }
 
+var parseResults = function (html) {
 
-var loadList = function(callback) {
+  var fails = 0,
+    locs, vals, result = {};
 
-    $.ajax({
-        url: 'https://raw.githubusercontent.com/dhowe/ChinaEye/master/sensitiveKeywords.txt',
-        type: 'get',
-        success: function(data) {
-            showLogs && console.log("Successfully get the list");
-            callback(processList(data));
-        },
-        error: function(e) {
-            callback({
-                status: 'error',
-                fails: -1
-            });
-            console.warn(e);
-        }
-    });
+  // remove img tags before calling find
+  html = html.replace(/<img\b[^>]*>/ig, '');
+  locs = $(html).find('.resultlocation');
+  vals = $(html).find('.resultstatus');
 
+  for (var i = 0; i < locs.length; i++) {
+
+    var value = $(vals[i]).text().toLowerCase();
+    result[$(locs[i]).text().toLowerCase()] = value;
+    if (value === 'fail') fails++;
+  }
+
+  result.status = fails > 2 ? 'block' : 'allow';
+  //console.log('result:', locs.length, vals.length, result);
+  return result;
 }
 
-var updateCheck = function() {
+var loadList = function (callback) {
 
-    var lastCheckTime = chrome.storage.local.get('lastCheckTime',function(data){
-        showLogs && console.log("lastCheckTime",data.lastCheckTime);
-        lastCheckTime = Date.parse(data.lastCheckTime);
-
-        var currentTime = Date.now();
-        var twelveHours = 1000* 60 * 60 * 12;
-
-        if (currentTime - lastCheckTime < twelveHours) {
-            showLogs && console.log("No need to update");
-            chrome.storage.local.get('list',function(data){
-               processTriggers(data.list);
-            });
-            return;
-        }
-        else loadList();   
-    });
-    
-   
-}
-
-var processTriggers = function(rules) {
-
-    for (var index in rules) {
-        var rule = rules[index];
-        var keywords = rule.replace(/ /g, "+").split("|");
-        triggers.push(keywords[0]);
-        if (keywords.length > 1) triggers.push(keywords[1]);
+  $.ajax({
+    url: 'https://raw.githubusercontent.com/dhowe/ChinaEye/master/sensitiveKeywords.txt',
+    type: 'get',
+    success: function (data) {
+      showLogs && console.log("Got list: " + data.length);
+      callback && callback(processList(data));
+    },
+    error: function (e) {
+      callback && callback({
+        status: 'error',
+        fails: -1
+      });
+      console.warn(e);
     }
-    showLogs && console.log('Triggers ready.');
+  });
 }
 
-var processList = function(list) {
-    var txtArr = list.split("\n");
+var updateCheck = function () {
 
-    var time = new Date().toLocaleString();
-    showLogs && console.log("Check time", time);
+  var lastCheckTime = chrome.storage.local.get('lastCheckTime', function (data) {
 
-    // var versionInfo = txtArr.filter(function (line) {
-    //   return /^! Version:(.*)/.test(line)});
-    // if(versionInfo && versionInfo.length > 0) 
-    //   var version = versionInfo[0].match(/^! Version:(.*)/)[1];
-    // showLogs && console.log("version",version);
+    showLogs && console.log("lastCheckTime", data.lastCheckTime);
 
-    var rules = txtArr.filter(function(line) {
-        return /^(?!!|\[).*/.test(line)
-    });
-    showLogs && console.log("Example", rules[1]);
+    lastCheckTime = Date.parse(data.lastCheckTime);
 
-    chrome.storage.local.set({ 'list': rules });
-    chrome.storage.local.set({ 'lastCheckTime': time });
+    var currentTime = Date.now(),
+      twelveHours = 1000 * 60 * 60 * 12;
 
-    processTriggers(rules);
+    if (currentTime - lastCheckTime < twelveHours) {
 
+      showLogs && console.log("No need to update");
+      chrome.storage.local.get('list', function (data) {
+        processTriggers(data.list);
+      });
+
+      return;
+
+    } else {
+
+      loadList();
+    }
+  });
 }
 
-var init = function() {
-    loadList();
+var processTriggers = function (rules) {
+
+  for (var index in rules) {
+
+    var rule = rules[index];
+    var keywords = rule.replace(/ /g, "+").split("|");
+    triggers.push(keywords[0]);
+    if (keywords.length > 1)
+      triggers.push(keywords[1]);
+  }
+  showLogs && console.log('Triggers ready.');
+}
+
+var processList = function (list) {
+
+  var txtArr = list.split("\n"),
+    time = new Date().toLocaleString();
+
+  showLogs && console.log("Check time", time);
+
+  var rules = txtArr.filter(function (line) {
+    return /^(?!!|\[).*/.test(line)
+  });
+
+  showLogs && console.log("Example", rules[1]);
+
+  chrome.storage.local.set({
+    list: rules
+  });
+
+  chrome.storage.local.set({
+    lastCheckTime: time
+  });
+
+  processTriggers(rules);
 }
--- js/content.js
+++ js/content.js
@@ -1,50 +1,60 @@
-console.log('Loaded content.js for: ' + location.href);
+var url = location.href; // store original
+
+console.log('content.js: ' + url);
 
 if (document.querySelector('#rd_style') === null) {
+
+  sendCheckPage();
+}
+
+function sendCheckPage() {
 
   chrome.runtime.sendMessage({
     what: "checkPage",
     location: location
-  }, function (res) {
+  }, postCheckPage);
+}
 
-    console.log('result: ',res);
-    
-    if (res && res.status === 'block') {
+function postCheckPage(res) {
 
-      // Create text for the CSS we need for our font
-      var redact, fontFace = '@font-face { font-family: Redacted; src: url("' +
-        chrome.extension.getURL('fonts/redacted-regular.woff') + '"); }';
+  if (res && res.status === 'block') {
 
-      // Create style tags and inject them into the page
-      $("<style id='rd_style' type='text/css'>")
-        .html(fontFace)
-        .appendTo("head");
+    console.log("block");
 
-      $("<style id='rd_img_style 'type='text/css'>")
-        .html('img, image {\n-webkit-filter: brightness(0);\n}')
-        .appendTo("head");
+    // Create text for the CSS we need for our font
+    var redact, fontFace = '@font-face { font-family: Redacted; src: url("' +
+      chrome.extension.getURL('fonts/redacted-regular.woff') + '"); }';
 
-      // Apply our font/color to all sub-elements
-      (redact = function (ele) {
-        var eles = $('*', ele);
-        eles.each(function (index) {
-          if (this.tagName !== 'SCRIPT' && this.tagName !== 'STYLE')
-            $(this).css('font-family', 'Redacted')
-            .css('background', 'none')
-            .css('color', '#000');
-        });
-      })('body');
+    // Create style tags and inject them into the page
+    $("<style id='rd_style' type='text/css'>")
+      .html(fontFace)
+      .appendTo("head");
 
-      // rerun our function when new nodes are inserted
-      document.addEventListener('DOMNodeInserted', function (e) {
-        // console.log('DOMNodeInserted');
-        redact(e.relatedNode);
+    $("<style id='rd_img_style 'type='text/css'>")
+      .html('img, image {\n-webkit-filter: brightness(0);\n}')
+      .appendTo("head");
+
+    // Apply our font/color to all sub-elements
+    (redact = function (ele) {
+      var eles = $('*', ele);
+      eles.each(function (index) {
+        if (this.tagName !== 'SCRIPT' && this.tagName !== 'STYLE')
+          $(this).css('font-family', 'Redacted')
+          .css('background', 'none')
+          .css('color', '#000');
       });
-    }
-  });
+    })('body');
+
+    // rerun our function when new nodes are inserted
+    document.addEventListener('DOMNodeInserted', function (e) {
+      // console.log('DOMNodeInserted');
+      redact(e.relatedNode);
+    });
+  }
 }
 
 chrome.runtime.onMessage.addListener(
+
   function (message, sender, callback) {
 
     if (message.what === "isActive") {
@@ -52,5 +62,10 @@ chrome.runtime.onMessage.addListener(
       callback({
         'active': (document.querySelector('#rd_style') != null)
       });
+    }
+    // compare updated URL to original URL
+    else if (message.what === "tabUpdate" && message.url !== url) {
+
+      sendCheckPage(); // if URL is programmatically changed, recheck the page
     }
   });
